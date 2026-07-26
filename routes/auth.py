168767b7135e0f17493158a2a8f07d34b4b1ac6d
Mohamed <mohamadnafe14@gmail.com>
2026-07-26 10:45:53 +0300

    adding login

--- routes/auth.py
+++ routes/auth.py
@@ -5,8 +5,9 @@ from fastapi import APIRouter, Depends, HTTPException
 from database import get_db
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
 router = APIRouter()
-@router.post("/signUp")
+@router.post("/signUp",status_code=201)
 def create_user(user: UserCreate,db:Session=Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
     if user_db:
@@ -21,4 +22,12 @@ def create_user(user: UserCreate,db:Session=Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+@router.post("/login")
+def login(user:UserLogin,db:Session=Depends(get_db)):
+    user_db = db.query(User).filter(User.email == user.email).first()
+    if not user_db:
+        raise HTTPException(status_code=400, detail="User does not exist")
+    if not bcrypt.checkpw(user.password.encode(), user_db.password):
+        raise HTTPException(status_code=400, detail="Invalid email or password")
     return user_db
